5bef343fe00bd1ea183b0362a2667eacd32c907f
jeffrey-chan <jachan.santiago@gmail.com>
2019-11-01 23:10:40 -0400

    add minimal parameters for training

--- train.py
+++ train.py
@@ -1,18 +1,18 @@
-from src.data import load_data
+from src.data import load_dataset
 from src.model import BruiseDetector
 
 
 # Load the data
-train_data, test_data = load_data()
+train_data, test_data = load_dataset(batch_size=1)
 
 
 # Load Model
 model = BruiseDetector()
 # Train Model
-model.fit(train_data)
+model.fit(train_data, batch_size=1, epochs=1)
 
 # Save Model
-model.save()
+# model.save()
 
 # Evaluate The model
-model.evaluate()
+# model.evaluate()
